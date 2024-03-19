001c6ff0c8d963ed34fda9f240210b38508388fe
leandrag20 <leandragottschalk20@augustana.edu>
2024-03-19 11:49:44 -0500

    Added files for add friends and instructions page
    Very rough versions, just wanted to make the files.

---
+++ transform66/lib/pages/add_friends_page.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:transform66/auth.dart';
+
+class InstructionsPage extends StatelessWidget {
+  InstructionsPage({Key? key}) : super(key: key);
+
+  final User? user = Auth().currentUser;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget _userUid(){
+    return Text(user?.email ??'User email');
+  }
+
+  Widget _signOutButton(){
+    return ElevatedButton(
+      onPressed: signOut,
+      child: const Text('Sign Out'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: null,
+    body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _userUid(),
+          _signOutButton()
+        ],
+      ),
+    )
+
+    );
+  }
+
+}
