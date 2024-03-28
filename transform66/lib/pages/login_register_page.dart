9fecaaefded8f3065484814a4a552b3ac41149c6
Riva Kansakar <rivakansakar21@augustana.edu>
2024-03-27 22:05:37 -0500

    Changes made to main page

--- transform66/lib/pages/login_register_page.dart
+++ transform66/lib/pages/login_register_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:transform66/auth.dart';
 import '../auth.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -11,110 +10,115 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-
-
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   String? errorMessage = '';
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
   bool isLogin = true;
 
-Future<void> signInWithEmailAndPassword() async{
-  try{
-    await Auth().signInWithEmailAndPassword(
-      email: _controllerEmail.text, 
-      password: _controllerPassword.text,);
-
-  } on FirebaseAuthException catch(e){
-    setState(() {
-      errorMessage = e.message;
-    });
-    
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
   }
-}
 
-Future<void> createUserWithEmailAndPassword() async{
-  try {
-    await Auth().createUserWithEmailAndPassword(
-      email: _controllerEmail.text,
-      password: _controllerPassword.text,
-    );
-  } on FirebaseAuthException catch(e){
-    setState(() {
-      errorMessage = e.message;
-    });
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
   }
-}
 
-Widget _title(){
-  return const Text('Transform66');
-}
+  Widget _title() {
+    return const Text('Transform66');
+  }
 
-Widget _entryField(
-  String title,
-  TextEditingController controller,){
+  Widget _entryField(String title, TextEditingController controller) {
     return TextField(
       controller: controller,
       decoration: InputDecoration(
         labelText: title,
       ),
     );
-}
+  }
 
-Widget _errorMessage(){
-  return Text(errorMessage == '' ? '': 'Humm ? $errorMessage');
-}
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
+  }
 
-Widget _submitButton(){
-  return ElevatedButton(
-    onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-    child: Text(isLogin ? 'Login' : 'Register'),
-  );
-}
+  Widget _submitButton() {
+    return ElevatedButton(
+      onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      child: Text(isLogin ? 'Login' : 'Register'),
+    );
+  }
 
-Widget _loginOrRegisterButton(){
-  return TextButton(
-    onPressed: (){
-      setState(() {
-        isLogin = !isLogin;
-      });
-    },
-    child: Text(isLogin ? 'Register instead' : 'Login instead'),
-  );
-}
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          isLogin = !isLogin;
+        });
+      },
+      child: Text(isLogin ? 'Register instead' : 'Login instead'),
+    );
+  }
 
   @override
-  void dispose(){
+  void dispose() {
     _controllerEmail.dispose();
     _controllerPassword.dispose();
     super.dispose();
-}
-
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: null,
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            _entryField('email', _controllerEmail),
-            _entryField('password', _controllerPassword),
-            _errorMessage(),
-            _submitButton(),
-            _loginOrRegisterButton(),
-          ],)
-      )
-
-    );
-
   }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _entryField('email', _controllerEmail),
+                _entryField('password', _controllerPassword),
+                _errorMessage(),
+                _submitButton(),
+                _loginOrRegisterButton(),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Image.asset(
+                'assets/images/Transform66.png', // Replace with your image asset path
+                height: 400, // Adjust the height as needed
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
