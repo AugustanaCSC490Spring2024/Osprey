22be3ac5324d16b4631d44ab5f5d43ec61bfdace
leandrag20 <leandragottschalk20@augustana.edu>
2024-04-01 21:00:36 -0500

    added actual calendar to calendar page

--- transform66/lib/pages/calendar_page.dart
+++ transform66/lib/pages/calendar_page.dart
@@ -1,21 +1,41 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:transform66/auth.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   Calendar({Key? key}) : super(key: key);
 
-  final User? user = Auth().currentUser;
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  late User? user;
+  late DateTime _selectedDay;
+  late CalendarFormat _calendarFormat;
+  late Map<DateTime, List<dynamic>> _events;
+  late TextEditingController _eventController;
+
+  @override
+  void initState() {
+    super.initState();
+    user = Auth().currentUser;
+    _selectedDay = DateTime.now();
+    _calendarFormat = CalendarFormat.month;
+    _events = {};
+    _eventController = TextEditingController();
+  }
 
   Future<void> signOut() async {
     await Auth().signOut();
   }
 
-  Widget _userUid(){
-    return Text(user?.email ??'User email');
+  Widget _userUid() {
+    return Text(user?.email ?? 'User email');
   }
 
-  Widget _calendarButton(){
+  Widget _calendarButton() {
     return ElevatedButton(
       onPressed: () {},
       child: const Text('Welcome to the calendar page'),
@@ -23,7 +43,7 @@ class Calendar extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -40,22 +60,45 @@ class Calendar extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-        ), 
+        ),
       ),
-    body: Container(
-      height: double.infinity,
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _calendarButton()
-        ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //_calendarButton(),
+            const Text(
+                'Your history:',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            SizedBox(height: 20),
+            TableCalendar(
+              focusedDay: _selectedDay,
+              firstDay: DateTime(1990),
+              lastDay: DateTime(2050),
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _selectedDay = focusedDay;
+              },
+              eventLoader: (day) {
+                // Return events for a particular day from _events map
+                return _events[day] ?? [];
+              },
+            ),
+          ],
+        ),
       ),
-    )
-
     );
   }
-
 }
