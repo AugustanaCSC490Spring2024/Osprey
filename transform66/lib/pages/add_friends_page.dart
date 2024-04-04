cf4ddaaca726a05871de69f1e794b8a21dd52ac9
leandrag20 <leandragottschalk20@augustana.edu>
2024-04-03 22:07:49 -0500

    added code to add friends page

--- transform66/lib/pages/add_friends_page.dart
+++ transform66/lib/pages/add_friends_page.dart
@@ -7,40 +7,20 @@ class AddFriends extends StatelessWidget {
 
   final User? user = Auth().currentUser;
 
-  Future<void> signOut() async {
-    await Auth().signOut();
-  }
-
-  Widget _userUid(){
-    return Text(user?.email ??'User email');
-  }
-
-  Widget _signOutButton(){
-    return ElevatedButton(
-      onPressed: signOut,
-      child: const Text('Sign Out'),
-    );
-  }
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-    body: Container(
-      height: double.infinity,
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _userUid(),
-          _signOutButton()
-        ],
+      appBar: AppBar(
+        title: const Text(
+          'Add friends page',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.teal,
       ),
-    )
-
     );
   }
-
 }
