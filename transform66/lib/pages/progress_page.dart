732a58b97052e302bcff704610dd3908576e37dc
leandrag20 <leandragottschalk20@augustana.edu>
2024-04-25 01:07:34 -0500

    ignore this, made a mistake so hard to start over

--- transform66/lib/pages/progress_page.dart
+++ transform66/lib/pages/progress_page.dart
@@ -15,168 +15,164 @@ class ProgressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: const Text(
-          'Transform66',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.teal,
-        actions: [
-          IconButton(
-              icon: const Icon(Icons.calendar_month_sharp),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Calendar(),
-                  ),
-                );
-              }),
-          PopupMenuButton<String>(
-            onSelected: (String result) {
-              if (result == 'Sign Out') {
-                Auth().signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()), // Navigate back to LoginPage
-                );
-              }
-            },
-            
-            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-              const PopupMenuItem<String>(
-                value: 'Sign Out',
-                child: Text('Sign Out'),
-                
-              ),
-            ],
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Align(
-          alignment: Alignment.center,
-          child: SizedBox(
-            width: 350,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/Transform66.png',
-                  width: 300,
-                  height: 300,
-                ),
-                //const SizedBox(height: 20),
-                const Text(
-                  '0/66 days completed',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const Text(
-                  'Your progress for today:',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const TaskWidget(
-                  taskName: 'Drink 1 gallon of Water'
-                ),
-                const TaskWidget(
-                  taskName: 'Read 10 pages of a non-fiction book'
-                ),
-                const TaskWidget(
-                  taskName: '45 min outdoor exercise',
-                ),
-                const TaskWidget(
-                  taskName: '3 pages of creative writing',
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AddFriends(),
-                        ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all(
-                        const TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      'Add Friends',
-                      style: TextStyle(
-                          color: Colors.black,
-                          decoration:
-                              TextDecoration.underline), // Change color to black
-                    ),
-                  ),// Add Friends button
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => InstructionsPage(),
-                        ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all(
-                        const TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      'Instructions',
-                      style: TextStyle(
-                          color: Colors.black,
-                          decoration:
-                              TextDecoration.underline), // Change color to black
-                    ),
-                  ), // Instructions button
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Testimonials(),
-                        ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all(
-                        const TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      'Testimonials',
-                      style: TextStyle(
-                          color: Colors.black,
-                          decoration:
-                              TextDecoration.underline), // Change color to black
-                    ),
-                  ), // Testimonials button
-                ])
-              ],
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          title: const Text(
+            'Transform66',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          backgroundColor: Colors.teal,
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.calendar_month_sharp),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Calendar(),
+                    ),
+                  );
+                }),
+            PopupMenuButton<String>(
+              onSelected: (String result) {
+                if (result == 'Sign Out') {
+                  Auth().signOut();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const LoginPage()), // Navigate back to LoginPage
+                  );
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  value: 'Sign Out',
+                  child: Text('Sign Out'),
+                ),
+              ],
+            ),
+          ],
         ),
-      )
-    );
+        body: SingleChildScrollView(
+          child: Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 350,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Transform66.png',
+                    width: 300,
+                    height: 300,
+                  ),
+                  //const SizedBox(height: 20),
+                  const Text(
+                    '0/66 days completed',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const Text(
+                    'Your progress for today:',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const TaskWidget(taskName: 'Drink 1 gallon of Water'),
+                  const TaskWidget(
+                      taskName: 'Read 10 pages of a non-fiction book'),
+                  const TaskWidget(
+                    taskName: '45 min outdoor exercise',
+                  ),
+                  const TaskWidget(
+                    taskName: '3 pages of creative writing',
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddFriends(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                          const TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Add Friends',
+                        style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration
+                                .underline), // Change color to black
+                      ),
+                    ), // Add Friends button
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => InstructionsPage(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                          const TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Instructions',
+                        style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration
+                                .underline), // Change color to black
+                      ),
+                    ), // Instructions button
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Testimonials(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                          const TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Testimonials',
+                        style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration
+                                .underline), // Change color to black
+                      ),
+                    ), // Testimonials button
+                  ])
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
 
@@ -242,5 +238,3 @@ class _TaskWidgetState extends State<TaskWidget> {
     );
   }
 }
-
-
