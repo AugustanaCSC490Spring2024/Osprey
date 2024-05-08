6273e1dcda4699941de2d1f6b21c04c0b70876eb
Stuti Shrestha <stutishrestha21@augustana.edu>
2024-05-07 20:38:44 -0500

    Updated instruction

--- transform66/lib/pages/new_users_page.dart
+++ transform66/lib/pages/new_users_page.dart
@@ -22,8 +22,8 @@ class StartedPage extends StatelessWidget {
                     "1. When you click the start transform 66 button, then you are directed to customizing your task.\n\n"
                     "2. Choose a couple of tasks that you want to commit to for 66 days.\n\n"
                     "3. Remember that if you miss a day you will be taken back to day 1 so don't forget to do your task and mark it down.\n\n"
-                    "4. Press Feed icon to keep yourself updated on what tasks your friends are committing.\n\n"
-                    "5. Press Calendar icon to view your progress. You can see what day you are on and how many days are remaining.\n\n"
+                    "4. Swipe right and you will see Feed to keep yourself updated on what tasks your friends are commiting.\n\n"
+                    "5. Swipe right and you will see Calendar to view your progress. You can see what day you are on and how many days are remaining.\n\n"
                     "6. Click three dots in the top right corner to sign out, it'll take you back to the login page.\n\n"
                     "7. If you click Friends icon, you will be directed to a page where you can request your friend. \n\n"
                     "8. After requesting, your friend will have a notification if they want to accept or reject the invitation.\n\n"
@@ -40,7 +40,7 @@ class StartedPage extends StatelessWidget {
             onPressed: () {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => EditNewUserPage()),
+                MaterialPageRoute(builder: (context) => const EditNewUserPage()),
               );
             },
             style: ElevatedButton.styleFrom(
