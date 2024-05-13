941ed84e93ad6810d2b06bcafd15e4736916f297
Stuti Shrestha <stutishrestha21@augustana.edu>
2024-05-13 00:13:18 -0500

    Added developer  pic to the assets folder

--- transform66/lib/pages/instructions_page.dart
+++ transform66/lib/pages/instructions_page.dart
@@ -29,7 +29,7 @@ class InstructionsPage extends StatelessWidget {
                 Text(
                   "1. When you click the start transform 66 button, then you are directed to customizing your task.\n\n"
                   "2. Choose a couple of tasks that you want to commit to for 66 days.\n\n"
-                  "3. Remember that if you miss a day you will be taken back to day 1 so don't forget to do your task and mark it down.\n\n"
+                  "3. Remember that if you miss a day you will be taken back to day 1 so don't forget to do your task and mark it down by 11:59 pm.\n\n"
                   "4. Swipe right and you will see Feed to keep yourself updated on what tasks your friends are commiting.\n\n"
                   "5. Swipe right and you will see Calendar to view your progress. You can see what day you are on and how many days are remaining.\n\n"
                   "6. Click three dots in the top right corner to signout, it'll take you back to the login page.\n\n"
