f16829ccdc4874f507cd2bafa31a7050a95f7d3b
Riva Kansakar <rivakansakar21@augustana.edu>
2024-05-17 14:56:35 -0500

    Added back button on register page

--- transform66/lib/pages/login_register.dart
+++ transform66/lib/pages/login_register.dart
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
         ),
       );
     } else {
-      Navigator.pushReplacement(
+      Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => VerifyEmailPage(),
