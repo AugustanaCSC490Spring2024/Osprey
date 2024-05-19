0ba026d67b93c6bd1a5e7104e5fb7a491e18f2e9
leandrag20 <leandragottschalk20@augustana.edu>
2024-05-19 15:47:26 -0500

    fixed feed posts/cleaned code

--- transform66/lib/widgets/friend_feed_widget.dart
+++ transform66/lib/widgets/friend_feed_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:transform66/firestore_actions/feed_firestore.dart';
 
 class FriendFeedWidget extends StatefulWidget {
@@ -30,27 +31,71 @@ class _FriendFeedWidgetState extends State<FriendFeedWidget> {
   final FeedFirestoreService ffs2 = FeedFirestoreService();
 
   @override
-  @override
   Widget build(BuildContext context) {
-    return Card(child: Padding(padding: const EdgeInsets.all(10),child: 
-      Column(children: [
-        Row(children: [
-          const CircleAvatar(radius: 20, backgroundImage: AssetImage("assets/images/Transform66.png"), backgroundColor: Colors.transparent),
-          const SizedBox(width: 10),
-          Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Text(widget.userName, style: const TextStyle(fontSize: 14, color: Colors.black)),
-                Text(widget.message, style: const TextStyle(fontSize: 16, color: Colors.black)),
-              ])
-        ],),
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(children: [
+        Row(
+          children: [
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.teal, 
+                ),
+                child: Center(
+                  child: Text(
+                    widget.userName
+                        .substring(0, 1)
+                        .toUpperCase(), 
+                    style: const TextStyle(
+                      fontSize: 16, 
+                      color: Colors.white, 
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.userName,
+                      style:
+                          const TextStyle(fontSize: 14, color: Colors.black)),
+                  Text(widget.message,
+                      style:
+                          const TextStyle(fontSize: 16, color: Colors.black)),
+                ])
+          ],
+        ),
         const SizedBox(height: 5),
         Image.asset(
-        "assets/${widget.imagePath}${widget.imageType}.jpg",
-        width: 300,
-        height: 300,
-        fit: BoxFit.cover,
-      ),
+          "assets/${widget.imagePath}${widget.imageType}.jpg",
+          width: 300,
+          height: 300,
+          fit: BoxFit.cover,
+        ),
         const SizedBox(height: 5),
-        Text(widget.date.toDate().toString().substring(0, 16))
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+                onTap: () {
+                  setState(() {
+                    widget.isLiked = !widget.isLiked;
+                  });
+                },
+                child: Icon(Icons.favorite,
+                    color: widget.isLiked ? Colors.red : Colors.grey,
+                    size: 40)),
+            Text(widget.date.toDate().toString().substring(0, 16)),
+          ],
+        )
       ]),
     ));
   }
--- transform66/lib/widgets/personal_feed_widget.dart
+++ transform66/lib/widgets/personal_feed_widget.dart
@@ -27,26 +27,48 @@ class _PersonalFeedWidgetState extends State<PersonalFeedWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: Padding(padding: const EdgeInsets.all(10),child: 
-      Column(children: [
-        Row(children: [
-          const CircleAvatar(radius: 20, backgroundImage: AssetImage("assets/images/Transform66.png"), backgroundColor: Colors.transparent),
-          const SizedBox(width: 10),
-          Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Text(widget.userName, style: const TextStyle(fontSize: 14, color: Colors.black)),
-                Text(widget.message, style: const TextStyle(fontSize: 16, color: Colors.black))
-              ])
-        ]),
-        const SizedBox(height: 5),
-        Image.asset(
-        "assets/${widget.imagePath}${widget.imageType}.jpg",
-        width: 300,
-        height: 300,
-        fit: BoxFit.cover
-      ),
-        const SizedBox(height: 5),
-        Text(widget.date.toDate().toString().substring(0, 16))
-      ])
-    ));
+    return Card(
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(children: [
+              Row(children: [
+                SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.teal,
+                    ),
+                    child: Center(
+                      child: Text(
+                        widget.userName.substring(0, 1).toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.userName,
+                          style: const TextStyle(
+                              fontSize: 14, color: Colors.black)),
+                      Text(widget.message,
+                          style: const TextStyle(
+                              fontSize: 16, color: Colors.black))
+                    ])
+              ]),
+              const SizedBox(height: 5),
+              Image.asset("assets/${widget.imagePath}${widget.imageType}.jpg",
+                  width: 300, height: 300, fit: BoxFit.cover),
+              const SizedBox(height: 5),
+              Text(widget.date.toDate().toString().substring(0, 16))
+            ])));
   }
 }
